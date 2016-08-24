8d55fa06b52fa827f8a211984fc708d7e03b6c8f
Upkar Lidder <lidderupk@gmail.com>
2016-08-23 21:07:39 -0700

    final solution

---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,10 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by ulidder on 8/23/16.
+ */
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
